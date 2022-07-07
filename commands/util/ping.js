f236e570e504ac9e4736fcf6e1d0348967311a0b
Lotilia <93188667+yueSenpai@users.noreply.github.com>
2022-07-07 10:00:33 +0200

    Update ping.js

--- commands/util/ping.js
+++ commands/util/ping.js
@@ -1,5 +1,4 @@
 const { MessageEmbed } = require("discord.js");
-const pretty = require('pretty-ms');
 
 module.exports = {
     name: 'ping',
@@ -14,21 +13,7 @@ module.exports = {
     cooldown: 0,
 
     runInteraction: async (client, interaction) => {
-        const tryPong = await interaction.reply({ content: "** **", fetchReply: true })
 
-        const embed = new MessageEmbed()
-            .setTitle("🏓 pong !")
-            .setThumbnail(client.user.displayAvatarURL())
-            .addFields(
-                [
-                    { name: `Latence API`, value: `\`\`\`${client.ws.ping}ms\`\`\``, inline: true },
-                    { name: `Latence BOT`, value: `\`\`\`${tryPong.createdTimestamp - interaction.createdTimestamp}ms\`\`\``, inline: true },
-                    { name: "Uptime:", value: `\`\`\`yml\nStatus : Online\nUptime : ${pretty(client.uptime)}\n\`\`\`` }
-                ],
-            )
-            .setTimestamp()
-            .setFooter({ text: interaction.user.username, iconURL: interaction.user.displayAvatarURL() })
-
-        interaction.editReply({ content: " ", embeds: [embed] });
+        interaction.editReply({ content: "pong" });
     }
 }
